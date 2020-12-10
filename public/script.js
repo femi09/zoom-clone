64cfa029b21f14cbd49036ad5dca80b79e08cd6c
Femi Omotayo <omotayooluwafemi66@gmail.com>
2020-12-10 19:08:02 +0100

    added some styling to the user interface, implemented a real time chat with socket.io and added the stop video and mute audio functionality

--- public/script.js
+++ public/script.js
@@ -13,7 +13,7 @@ let myVideoStream;
 
 navigator.mediaDevices
   .getUserMedia({
-    video: false,
+    video: true,
     audio: true,
   })
   .then((stream) => {
@@ -51,4 +51,74 @@ const addVideoStream = (video, stream) => {
     video.play();
   });
   videoGrid.append(video);
+};
+
+let text = $("input");
+
+$("html").keydown((e) => {
+  if (e.which == 13 && text.val().length !== 0) {
+    console.log(text.val());
+    socket.emit("message", text.val());
+    text.val("");
+  }
+});
+
+socket.on("createMessage", (message) => {
+  $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
+  scrollToBottom = () => {
+    let d = $(".main__chat_window");
+    d.scrollTop(d.prop("scrollHeight"));
+  };
+});
+
+// Mute Video
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnMuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+};
+
+const setMuteButton = () => {
+  const html = `<i class=fas fa-microphone></i>
+      <span>Mute</span>
+      `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setUnMuteButton = () => {
+  const html = `<i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+    `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+// Stop Video
+const playStop = () => {
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo();
+  } else {
+    setStopVideo();
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+};
+
+const setStopVideo = () => {
+  const html = `<i class="fas fa-video"></i>
+    <span>Stop Video</span>
+    `;
+  document.querySelector(".main__video_button").innerHTML = html;
+};
+
+const setPlayVideo = () => {
+  const html = `<i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+    `;
+  document.querySelector(".main__video_button").innerHTML = html;
 };
